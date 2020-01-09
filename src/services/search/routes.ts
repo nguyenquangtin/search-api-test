7bf5578368f99915ab7a8ff79ba1255d5b3eab39
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2020-01-09 09:29:13 +0700

    Add the search controller

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
+import { getPlacesByName } from "./SearchController";
 
 export default [
   {
-    path: "/",
+    path: "/api/v1/search",
     method: "get",
-    handler: async (req: Request, res: Response) => {
-      res.send("Hello World!");
-    }
+    handler: [
+      async ({ query }: Request, res: Response) => {
+        const result = await getPlacesByName(query.q);
+        res.status(200).send(result);
+      }
+    ]
   }
 ];
